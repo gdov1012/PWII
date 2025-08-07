60a31ee7277b4348e0d419f8d745844ddbcbf85e
gdov1012 <valerogiovanna7@gmail.com>
2025-08-07 20:32:05 -0300

    Add files via upload

---
+++ PHP/ativi2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $n1 = 900;
+    $n2 = 20;
+    $soma = $n1 + $n2;
+    echo "A soma de $n1 e $n2 é: " . $soma;
+    ?>
+</body>
+</html>
