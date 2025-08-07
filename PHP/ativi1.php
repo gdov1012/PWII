7aaff76decf14abe97d999e0d9da244b2f29102e
gdov1012 <valerogiovanna7@gmail.com>
2025-08-07 20:28:07 -0300

    Add files via upload

---
+++ PHP/ativi1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <?php
+    $nome = "Giovanna Valero";
+    $idade = "16";
+
+    echo "Meu nome é $nome e eu tenho $idade ";
+    ?>
+</body>
+</html>
