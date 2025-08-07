5eb493e1d4b3f076b7c1c61074fc28bec7cfe2b1
gdov1012 <valerogiovanna7@gmail.com>
2025-08-07 20:41:52 -0300

    Add files via upload

---
+++ PHP/ativi3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 3</title>
+</head>
+<body>
+    <?php
+    $nome1 = "Ivy";
+    $nome2 = "Isabelle";
+
+    echo "$nome1 e $nome2 são irmãs!";
+    ?>
+</body>
+</html>
