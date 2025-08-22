9a9de1795f1590efbea1156c3a1c6237059d007a
gdov1012 <valerogiovanna7@gmail.com>
2025-08-21 22:27:15 -0300

    Add files via upload

---
+++ form_login/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form action="/processamento_form_login.php" method="post">
+        Login <br>
+        <input type="text" name="nomeusuario"></input><br>
+        Senha <br>
+        <input type="password" name="senhausuario"></input><br>
+        <input type="submit"></input><br>
+    </form>
+</body>
+</html> 
---
+++ form_login/processamento_form_login.php
@@ -0,0 +1,7 @@
+<?php
+$nomedousuario = $_POST['nomeusuario'];
+$pass = $_POST['senhausuario'];
+ echo "<p>Processador de Formulário Login</p><br>";
+ echo $nomedousuario . " " . $pass;
+?>
+
