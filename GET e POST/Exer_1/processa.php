c5da8bde2a683cd4ae80897871348d5c0fc8be35
gdov1012 <valerogiovanna7@gmail.com>
2025-10-30 21:29:00 -0300

    Delete GET e POST directory

--- GET e POST/Exer_1/processa.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// Verifica se o formulário foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-// Acessa o dado enviado via POST
-$nome = $_POST['nome'];
-echo"Olá," . $nome . "!";
-}
-?>
